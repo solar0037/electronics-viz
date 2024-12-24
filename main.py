58c3daf73f6a366725436318ada5580bf361955e
solar0037 <solar0037@gmail.com>
2024-12-24 20:10:19 +0900

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def main():
+    st.set_page_config("전자회로 시뮬레이터")
+    st.title("전자회로 시뮬레이터")
+    st.write("아날로그/디지털 회로를 시각적으로 상호작용할 수 있게 만든 프로젝트입니다.")
+    st.write("사용 기술: Python, Streamlit, Schemdraw 등")
+    st.link_button("GitHub", "https://github.com/solar0037/whatilearned")
+
+
+if __name__ == '__main__':
+    main()
