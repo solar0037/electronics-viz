22348cb5c9dc173006ac5e8d1b6ee8fa6996e3fa
solar0037 <solar0037@gmail.com>
2025-01-05 18:42:25 +0900

    add rc, rl circuit interactivity

--- circuits/rc_lpf.py
+++ circuits/rc_lpf.py
@@ -5,51 +5,80 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-st.title("RC LPF")
 
-with schemdraw.Drawing() as d:
-    V = elm.SourceV().label('$v_{i}$')
-    Res = elm.Resistor().right().label('R')
-    Cap = elm.Capacitor().down(d.unit).at(Res.end).label('C')
-    elm.Line().to(V.start)
-    plus = elm.Line().right(d.unit/2).at(Res.end).dot(open=True)
-    elm.Gap().down().label(['+','$v_C$','-']).at(plus.end)
-    elm.Line().right(d.unit/2).at(Cap.end).dot(open=True)
+@st.cache_data
+def draw_plots():
+    st.title("RC LPF")
 
-d.draw()
-st.pyplot(plt.gcf())
+    with schemdraw.Drawing() as d:
+        V = elm.SourceV().label('$v_{i}$')
+        Res = elm.Resistor().right().label('R')
+        Cap = elm.Capacitor().down(d.unit).at(Res.end).label('C')
+        elm.Line().to(V.start)
+        plus = elm.Line().right(d.unit/2).at(Res.end).dot(open=True)
+        elm.Gap().down().label(['+','$v_C$','-']).at(plus.end)
+        elm.Line().right(d.unit/2).at(Cap.end).dot(open=True)
 
-cols = st.columns(2)
+    d.draw()
+    st.pyplot(plt.gcf())
 
-frequencies = np.logspace(0, 6, 1000)
-omega = 2 * np.pi * frequencies
+    cols = st.columns(2)
 
-V_in = 1
-R = 1e3
-C = 10e-9
-Z_C = 1 / (1j*omega*C)
-f_c = 1/(2*math.pi*R*C)
+    frequencies = np.logspace(0, 6, 1000)
+    omega = 2 * np.pi * frequencies
 
-H = Z_C / (R + Z_C)
-magnitude = np.abs(H)
-phase = np.angle(H, deg=True)
+    V_in = 1
+    R = 1e3
+    C = 10e-9
+    Z_C = 1 / (1j*omega*C)
+    f_c = 1/(2*math.pi*R*C)
 
-plt.clf()
-plt.semilogx(frequencies, magnitude, label=r'$|H|$')
-plt.axvline(f_c, color='r', linestyle='--', label='Cutoff Frequency')
-plt.title("Transfer Function ($|H|$)")
-plt.xlabel("Frequency (Hz)")
-plt.ylabel("|H| (V/V)")
-plt.legend()
+    H = Z_C / (R + Z_C)
+    magnitude = np.abs(H)
+    phase = np.angle(H, deg=True)
 
-cols[0].pyplot(plt.gcf())
+    plt.clf()
+    plt.semilogx(frequencies, magnitude, label=r'$|H|$')
+    plt.axvline(f_c, color='r', linestyle='--', label='Cutoff Frequency')
+    plt.title("Transfer Function ($|H|$)")
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("|H| (V/V)")
+    plt.legend()
 
-plt.figure(2)
-plt.semilogx(frequencies, phase, label=r'$θ$')
-plt.axvline(f_c, color='r', linestyle='--', label='Cutoff Frequency')
-plt.title("Phase Angle (θ)")
-plt.xlabel("Frequency (Hz)")
-plt.ylabel("θ (˚)")
-plt.legend()
+    cols[0].pyplot(plt.gcf())
+    plt.clf()
 
-cols[1].pyplot(plt.gcf())
+    plt.semilogx(frequencies, phase, label=r'$θ$')
+    plt.axvline(f_c, color='r', linestyle='--', label='Cutoff Frequency')
+    plt.title("Phase Angle (θ)")
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("θ (˚)")
+    plt.legend()
+
+    cols[1].pyplot(plt.gcf())
+
+
+draw_plots()
+
+
+def transfer_function(f: int | float) -> list[float, float]:
+    V_in = 1
+    R = 1e3
+    C = 10e-9
+    Z_C = 1 / (1j*2*math.pi*f*C)
+
+    H = Z_C / (R + Z_C)
+    magnitude = abs(H) / V_in
+    phase = np.angle(H, deg=True)
+    return magnitude, phase
+
+
+f = st.select_slider(r'$f (\mathrm Hz)$', [1, 10, 1e2, 1e3, 1e4, 15.92e3, 1e5, 1e6])
+res = transfer_function(f)
+st.write(f'#### Signal Attenuation: {'Yes' if f>1e3 else 'No'}')
+st.write(r'$H(f) = ' + f'{res[0]:.2f}' + r' \mathrm V/\mathrm V$')
+st.write(r'$\theta = ' + f'{int(res[1])}' + r' \mathrm \degree$')
+st.write('### Parameters')
+st.write(r'$R = 1\mathrm k \Omega$')
+st.write(r'$C = 10 \mathrm n \mathrm F$')
+st.write(r'$f_c = 15.92\mathrm{kHz}$')
