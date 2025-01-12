9d05328d9a193efa9a6abf75e55ac57397bfa597
solar0037 <solar0037@gmail.com>
2025-01-12 16:58:24 +0900

    add kvl, kcl

---
+++ circuits/kvl_kcl.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import schemdraw
+from schemdraw import elements as elm
+import matplotlib.pyplot as plt
+
+st.title("KVL/KCL (Kirchhoff's Voltage/Current Law)")
+
+@st.cache_data
+def draw_kvl_circuit():
+    with schemdraw.Drawing() as d:
+        elm.SourceV().label(['-','$V$','+'])
+        elm.Resistor().right(d.unit*1.5).label(['+','$V_1$','-'])
+        elm.Resistor().down().label(['+','$V_2$','-'], loc='bot')
+        elm.Line().left(d.unit*1.5)
+    st.pyplot(plt.gcf())
+
+
+@st.cache_data
+def draw_kcl_circuit():
+    with schemdraw.Drawing() as d:
+        elm.SourceV()
+        L1 = elm.Line().right()
+        elm.CurrentLabel().right().at(L1).label('$I_1$')
+        R1 = elm.Resistor()
+        elm.CurrentLabel().right().at(R1).label('$I_2$')
+        elm.Line().down()
+        elm.Line().left()
+        R2 = elm.Resistor().down().at(L1.end)
+        elm.CurrentLabel().down().at(R2).label('$I_3$')
+        elm.Line().left()
+    st.pyplot(plt.gcf())
+
+st.header('KVL (Kirchhoff\'s Voltage Law)')
+draw_kvl_circuit()
+st.write(r'$V = 5\mathrm{V}$')
+st.write('$V = V_1 + V_2$')
+V_1 = st.slider(r'$V_1 (\mathrm{V})$', 0.0, 5.0, 1.0)
+st.write('$V_2 = ' + f'{5-V_1:.2f}' + r'\mathrm{V}$')
+
+st.header('KCL (Kirchhoff\'s Current Law)')
+draw_kcl_circuit()
+st.write(r'$I_1 = 0.1\mathrm{mA}$')
+st.write('$I_1 = I_2 + I_3$')
+I_2 = st.slider(r'$I_2 (\mathrm{mA})$', 0.0, 0.1, 0.01)
+st.write('$I_3 = ' + f'{0.1-I_2:.2f}' + r'\mathrm{mA}$')
+
+st.header('Description')
+st.write('### KVL: $\sum{V} = 0$')
+st.write('### KCL: $\sum{I} = 0$')
+st.write('')
--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import streamlit as st
 
 def main():
     home_page = st.Page("circuits/home.py", title="Home", icon=":material/home:")
+    kvl_kcl_page = st.Page("circuits/kvl_kcl.py", title="KVL/KCL")
     rc_lpf_page = st.Page("circuits/rc_lpf.py", title="RC LPF")
     rl_hpf_page = st.Page("circuits/rl_hpf.py", title="RL HPF")
     op_amp_page = st.Page("circuits/op_amp.py", title="Op Amp")
     diode_page = st.Page("circuits/diode.py", title="Diodes")
     mosfet_page = st.Page("circuits/mosfet.py", title="MOSFET")
     common_source_amp_page = st.Page("circuits/common_source_amp.py", title="Common Source Amplifier")
-    pg = st.navigation([home_page, rc_lpf_page, rl_hpf_page, op_amp_page, diode_page, mosfet_page, common_source_amp_page])
+    pg = st.navigation([home_page, kvl_kcl_page, rc_lpf_page, rl_hpf_page, op_amp_page, diode_page, mosfet_page, common_source_amp_page])
     pg.run()
 
 
